149411e7d6e214e9b066f9b1f5826f43816a968d
Manuela <manuelamunozlujan0@gmail.com>
2025-03-17 17:45:06 -0500

    buscador finalizado

--- js/listado-pro.js
+++ js/listado-pro.js
@@ -161,7 +161,37 @@ let searchProductTable = () => {
     //obtener lo escrito en el campo de texto
     let textSearch = searchInput.value.toLowerCase();
     // console.log(textSearch);
+    clearDataTable();
+    let i = 0
     for (let pro of products) {
-        
+        //comprobar coincidencia de los productos
+        if (pro.nombre.toLowerCase().indexOf(textSearch) != -1){
+            // console.log("Encontré algo");
+            let row = d.createElement("tr");
+                row.innerHTML = `
+                <td>${i++}</td>
+                <td>${pro.nombre}</td>
+                <td>${pro.descripcion}</td>
+                <td>${pro.precio}</td>
+                <td>${pro.stock}</td>
+                <td> <img src ="${pro.imagen}" width="100"></td>
+                <td>
+                    <button id="btn-edit" onclick="editDataTable(${i})" type="button" class="btn btn-warning">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                        </svg>
+                    </button> 
+                    ${ nameUser.textContent == "vendedor" ? "" : 
+                    `<button id="btn-delete" onclick="deleteDataTable(${i})" type="button" class="btn btn-danger">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+                        </svg>
+                    </button>`}
+                </td>
+                `;
+                tablePro.appendChild(row);
+        }
     }
 };
